bf1fe61ed5cf896a6e5f75f337963631b566ca31
Marlon De León <ivandmarlon@gmail.com>
2023-12-04 10:30:47 -0600

    implemented exec procedure and formatted

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from db.connection import get_connection
 from utility.file_utils import get_dir_filnames
 
+
 def run():
-    filepaths = get_dir_filnames('./exported')
+    filepaths = get_dir_filnames("./exported")
     conn = get_connection()
+    cursor = conn.cursor()
+    stored_proc_name = "spWT_Geofences_ImportGeofenceByJson"
 
-    for f in filepaths:
-        with open(f, 'r', encoding='utf-8-sig' ) as f:
-            json_geofences = f.readlines()
+    try:
+        for f in filepaths:
+            with open(f, "r", encoding="utf-8-sig") as f:
+                json_geofences = f.readlines()
 
-            # exec procedure
+                # exec procedure
+                cursor.execute(
+                    f"""
+                    EXEC {stored_proc_name}
+                        @userId=-1,
+                        @organizationId=?,
+                        @jsonList='{json_geofences}'
+                    """
+                )
 
+    except Exception as e:
+        print(e)
+
+    cursor.close()
     conn.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
